fa97a0911ec5efd0a51ef903357f58a67b41d3ec
ikowalec <aikey2011@gmail.com>
2020-11-03 16:59:37 +0000

    Now added saving previous results in new folders

--- scripts/CELL_steps/6_MC_settings.py
+++ scripts/CELL_steps/6_MC_settings.py
@@ -1,10 +1,13 @@
 # Starting MC simulations
 # Show sublattice properties
-
 from clusterx.model import ModelBuilder
 from clusterx.clusters.clusters_pool import ClustersPool
 from clusterx.structures_set import StructuresSet
 from clusterx.super_cell import SuperCell
+
+
+# Read the previously generated structures_set from .json database file
+sset = StructuresSet(db_fname="sset.json")
 
 scell = SuperCell(json_db_filepath="scell.json")
 scell.get_sublattice_types(pretty_print=True)
@@ -45,6 +48,14 @@ for n in range(1, len(scell)):
 
 # Write out the new folder structure and generate a file cointaining the list of folders
 sset.write_files(prefix="sset_mc")
+# Include previously calculated values in energy.dat file
+for i in range(0, len(sset)):
+        property_name = "energy"
+        e_model = sset.get_property_values(property_name)[i]
+        f = open(sset.get_folders()[i]+"/"+property_name+".dat", "w")
+        f.write(str(e_model))
+        f.close()
+
 
 
 # Write the database once all MonteCarlo simulations have finished
@@ -52,6 +63,7 @@ sset.serialize("sset_mc.json")
 
 structure_locations = sset.get_folders()
 sset_write_locations = open("folder_paths.txt", "w")
+
 for i in structure_locations:
     if i is not structure_locations[-1]:
         sset_write_locations.write(i+" ")
