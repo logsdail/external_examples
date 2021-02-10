2d5b3d345873346178ed21a026327a0dc06b8e30
lkabalan <Lkabalan@cardiff.ac.uk>
2021-02-10 17:32:17 +0000

    saving the predicted structures

--- scripts/CELL_steps/4_CE_plot_property_vs_concentration.py
+++ scripts/CELL_steps/4_CE_plot_property_vs_concentration.py
@@ -13,7 +13,7 @@ def read_property(i, folder, structure=None, **kwargs):
     print(folder, erg)
     return erg
 
-prefix = "sset_mc"
+prefix = "sset"  #to be changed to sset_mc if needed
 property_name = "energy"
 sset = StructuresSet(db_fname=prefix+".json")
 sset.read_property_values(property_name, write_to_file=False, read_property=read_property)
